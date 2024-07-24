3c3b0bfefe63137185a6959e38dcd87ac48b0571
WENYU CHIU <chiuwenyu@gmail.com>
2024-07-24 16:20:03 +0800

    Add FileButton component and fileName label

--- src/components/hydraulic-page/Thermo.tsx
+++ src/components/hydraulic-page/Thermo.tsx
@@ -1,12 +1,68 @@
-import { Box } from "@mui/material";
+import { Box, Grid, Stack, Typography } from "@mui/material";
 import VerticalLinearStepper from "./VerticalLinearStepper";
+import FileButton from "../single-page/FileButton";
+import { useState } from "react";
+import FolderOpenIcon from "@mui/icons-material/FolderOpen";
 
 const Thermo = () => {
+  const [fileName, setFileName] = useState("");
+
+  const onNewButtonClick = () => {
+    console.log("New button clicked");
+  };
+  const onOpenButtonClick = () => {
+    console.log("Open button clicked");
+  };
+  const onSaveButtonClick = () => {
+    console.log("Save button clicked");
+  };
+  const onSaveAsButtonClick = () => {
+    console.log("Save As button clicked");
+  };
+  const onExportButtonClick = () => {
+    console.log("Export button clicked");
+  };
+
   return (
     <>
-      <Box width={275}>
-        <VerticalLinearStepper />
-      </Box>
+      <Stack direction="row" spacing={1.5} marginBottom={"20px"}>
+        <FileButton
+          onNewButtonClick={onNewButtonClick}
+          onOpenButtonClick={onOpenButtonClick}
+          onSaveButtonClick={onSaveButtonClick}
+          onSaveAsButtonClick={onSaveAsButtonClick}
+          onExportButtonClick={onExportButtonClick}
+        />
+        {fileName !== "" && (
+          <Stack
+            direction={"row"}
+            spacing={1}
+            alignItems={"center"}
+            color={"primary.main"}
+          >
+            <FolderOpenIcon />
+            <Typography variant="body2">{fileName}</Typography>
+          </Stack>
+        )}
+      </Stack>
+      <Grid>
+        <Typography
+          gutterBottom
+          variant="h5"
+          component="div"
+          sx={{ fontWeight: "medium" }}
+        >
+          Thermosyphon Hydraulic Check
+        </Typography>
+        <Box
+          border={1}
+          width={275}
+          sx={{ borderColor: "grey.400", boxShadow: 3, padding: 2, mt: 3 }}
+        >
+          <Typography sx={{ mb: 3 }}>WORKING PROCEDURE :</Typography>
+          <VerticalLinearStepper />
+        </Box>
+      </Grid>
     </>
   );
 };
